3977d63743f6d0681adf904907f42eb75139a618
Henry <henryalejandro1229@gmail.com>
2026-08-25 14:53:46 -0600

    [feat] records para respuesta de servicio reservas

---
+++ commons/src/main/java/com/henry/commons/dto/huespedes/DataHuesped.java
@@ -0,0 +1,10 @@
+package com.henry.commons.dto.huespedes;
+
+public record DataHuesped(
+        String nombre,
+        //String nacionalidad,
+        String email,
+        String telefono
+        //String documento
+) {
+}
