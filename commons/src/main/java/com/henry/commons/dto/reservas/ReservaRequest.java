254272d80661b5a5d85e468bf4f56af2383d1701
FelixRivera123 <felixriveraromero777>
2026-08-27 19:13:06 -0600

    [fix] implementa logica para modulo Reserva

--- commons/src/main/java/com/henry/commons/dto/reservas/ReservaRequest.java
+++ commons/src/main/java/com/henry/commons/dto/reservas/ReservaRequest.java
@@ -1,16 +1,30 @@
 package com.henry.commons.dto.reservas;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
+import java.time.LocalDateTime;
+
 public record ReservaRequest(
 
-    @NotNull(message = "La fecha de inicio es requerida")
-    @Positive(message = "La fecha de inicio debe ser positiva")
+    @NotNull(message = "La id del huesped es requerida")
+    @Positive(message = "El id del huésped debe ser positivo")
     Long idHuesped,
 
-    @NotNull(message = "La fecha de inicio es requerida")
-    @Positive(message = "La fecha de inicio debe ser positiva")
-    Long idHabitacion
+    @NotNull(message = "El id de la habitacion es requerida")
+    @Positive(message = "El id del la habitacion debe ser positivo")
+    Long idHabitacion,
+
+    @NotNull(message = "La fecha de entrada es requerida")
+    @FutureOrPresent(message = "La fecha de entrada debe ser actual o futura")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm")
+    LocalDateTime fechaEntrada,
+
+    @NotNull(message = "La fecha de la salida es requerida")
+    @FutureOrPresent(message = "La fecha de salida debe ser actual a futura")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm")
+    LocalDateTime fechaSalida
 ) {
 }
